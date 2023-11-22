01483826f3eac031e62d7d7473257bb041060b56
Jules <julek.wielki@gmail.com>
2023-11-22 22:23:41 +0100

    plik przykładowy

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+from tkinter import filedialog
 
-# Press F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import wfdb as wfdb
+from matplotlib import pyplot as plt
+from numpy import *
+import csv
+
+# """
+file = filedialog.askopenfilename(initialdir=".", filetypes=[("Plik DAT", "*.dat")])
+filename = str(file)
+name = filename[0:len(filename)-4]
+print(name)
+# """
+
+# name = "132022-2023-04-27-10-44-55_MG_ramp"
+
+# """
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# ecg_data_raw = wfdb.rdsamp("132022-2023-04-27-10-44-55_MG_ramp")
+ecg_data_raw = wfdb.rdrecord(name)
 
+"""
+print(len(ecg_data_raw))
+print(ecg_data_raw[1])
+print(len(ecg_data_raw[0]))
+print(len(ecg_data_raw[0][0]))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+dane = [[], [], [], [], [], [], [], [], [], [], [], []]
+for a in ecg_data_raw[0]:
+    for x in range(len(a)):
+        dane[x].append(a[x])
+"""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+zapis = []
+# """
+print(ecg_data_raw)
+for i in range(len(ecg_data_raw.p_signal[0])):
+    dane = []
+    for a in range(len(ecg_data_raw.p_signal)):
+        dane.append(ecg_data_raw.p_signal[a][i])
+    zapis.append(dane)
+freq = ecg_data_raw.fs
+print(freq)
+e = ecg_data_raw
+print(e.n_sig)
+print(e.fmt)
+print(e.baseline)
+
+print(len(zapis))
+print(len(zapis[0]))
+
+fig, axes = plt.subplots(3, 4)
+
+for i in range(len(axes)):
+    for j in range(len(axes[0])):
+        axes[i][j].plot(zapis[j + i * 4][42:])
+        axes[i][j].set_title("lead " + str(j + i * 4 +1))
+
+plt.show()
+
+# """
+
